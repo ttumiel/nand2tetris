b86364fbdc624ed4621a0242154e1115bc441408
Thomas Tumiel <ttumiel@gmail.com>
2020-06-11 16:42:13 +0200

    clean up and add cli

--- projects/07/translator.py
+++ projects/07/translator.py
@@ -25,11 +25,8 @@ A=M-1 // SP--
 # Apply an arithmetic operation to 2 args
 arth_2_args = "M=M{op}D // *SP+D"
 
-
 # Apply a logic operation to 2 args
-log_2_args = "D=M-D"
-
-log_cmp = """
+log_2_args = """D=M-D
 @LOG_{cmp}_true.{uuid}
 D;J{cmp}
 @SP
@@ -41,29 +38,25 @@ M=0
 @SP
 A=M-1
 M=-1
-(LOG_{cmp}_false.{uuid})
-"""
+(LOG_{cmp}_false.{uuid})"""
 
-arth_1_arg = "M={op}M // *SP=D"
+# apply an operation to 1 arg
+asm_1_arg = "M={op}M // *SP=D"
 
-log_1_arg = "D=M" # Unused
-
-
-def one_arg_asm(*args, logic=True, **kwargs):
-    asm = load_1_arg
-    asm += (log_1_arg+log_cmp) if logic else arth_1_arg
+def one_arg_asm(*args, **kwargs):
+    asm = load_1_arg + asm_1_arg
     return asm.format(*args, **kwargs)
 
 def two_arg_asm(*args, logic=True, extra="", **kwargs):
     asm = load_2_args
-    asm += (log_2_args+log_cmp) if logic else arth_2_args
+    asm += log_2_args if logic else arth_2_args
     return asm.format(*args, extra=extra, **kwargs)
 
 arithmetic_operations = {
     # arithmetic
     'add': lambda _: two_arg_asm(op='+', logic=False),
     'sub': lambda _: two_arg_asm(op='-', logic=False),
-    'neg': lambda _: one_arg_asm(op='-', logic=False),
+    'neg': lambda _: one_arg_asm(op='-'),
 
     # logical
     'eq': lambda uuid: two_arg_asm(cmp='EQ', uuid=uuid),
@@ -73,10 +66,10 @@ arithmetic_operations = {
     # These work if the boolean arguments are _always_ represented as -1/0.
     'and': lambda _: two_arg_asm(op='&', logic=False),
     'or': lambda _: two_arg_asm(op='|', logic=False),
-    'not': lambda uuid: one_arg_asm(op='!', logic=False)
+    'not': lambda uuid: one_arg_asm(op='!')
 }
 
-push_op = """@{segment}
+seg_push = """@{segment}
 D=M // D=base_addr
 @{addr}
 A=D+A // A=base_addr+offset
@@ -85,11 +78,9 @@ D=M // D=val
 A=M
 M=D
 @SP
-M=M+1
-"""
+M=M+1"""
 
-pop_op = """
-@{segment}
+seg_pop = """@{segment}
 D=M // D=base_addr
 @{addr} // offset
 D=D+A // D = base_addr+offset
@@ -100,78 +91,44 @@ AM=M-1 //SP--
 D=M // D=val
 @R13
 A=M
-M=D
-"""
+M=D"""
 
-static_pop = """
-@SP
+address_pop = """@SP
 AM=M-1
 D=M
-@Static.{i}
-M=D
-"""
+@{addr}
+M=D"""
 
-static_push = """
-@Static.{i}
-D=M
+address_push = """@{addr}
+D={val}
 @SP
 A=M
 M=D
 @SP
-M=M+1
-"""
+M=M+1"""
 
-tmp_ptr = {'pointer': 3, 'temp': 5}
-
-ptr_tmp_pop = """
-@SP
-AM=M-1 //SP--
-D=M // D=val
-@{tmp_ptr} // +i
-M=D
-"""
-
-ptr_tmp_push = """
-@{tmp_ptr} // +i
-D=M // D=val
-@SP
-A=M
-M=D
-@SP
-M=M+1
-"""
-
-cnst_push = """
-@{constant}
-D=A // D=val
-@SP
-A=M
-M=D
-@SP
-M=M+1
-"""
-
-push_segments = {'local': 'LCL', 'argument': 'ARG', 'this': 'THIS', 'that': 'THAT'} # add vars pointer=3, temp=5
+addr_segments = {
+    'pointer':  lambda i: {'addr':3+int(i),    'val':'M'},
+    'temp':     lambda i: {'addr':5+int(i),    'val':'M'},
+    'constant': lambda i: {'addr':i,           'val':'A'},
+    'static':   lambda i: {'addr':'Static.'+i, 'val':'M'}
+}
+push_segments = {'local': 'LCL', 'argument': 'ARG', 'this': 'THIS', 'that': 'THAT'}
 
 # remove duplicate
 def push(segment, offset):
     if segment in push_segments:
-        return push_op.format(segment=push_segments[segment], addr=offset)
-    elif segment in tmp_ptr:
-        return ptr_tmp_push.format(tmp_ptr=tmp_ptr[segment] + int(offset))
-    elif segment == 'constant':
-        return cnst_push.format(constant=offset)
-    elif segment == 'static':
-        return static_push.format(i=offset)
+        return seg_push.format(segment=push_segments[segment], addr=offset)
+    elif segment in addr_segments:
+        return address_push.format(**addr_segments[segment](offset))
     else: raise ValueError(segment, offset)
 
 def pop(segment, offset):
+    assert segment != 'constant'
     if segment in push_segments:
-        return pop_op.format(segment=push_segments[segment], addr=offset)
-    elif segment in tmp_ptr:
-        return ptr_tmp_pop.format(tmp_ptr=tmp_ptr[segment] + int(offset))
-    elif segment == 'static':
-        return static_pop.format(i=offset)
+        return seg_pop.format(segment=push_segments[segment], addr=offset)
+    elif segment in addr_segments:
+        return address_pop.format(**addr_segments[segment](offset))
     else: raise ValueError(segment, offset)
 
 
@@ -195,20 +152,18 @@ def get_lines(filename):
                 if len(line)>0:
                     yield line
 
-def translator(filename):
-    print(filename[:-2] + 'asm')
-    with open(filename[:-2] + 'asm', 'w') as f:
+def translator(filename, fileout=None, annotate=False):
+    fileout = fileout or filename[:-2] + 'asm'
+    with open(fileout, 'w') as f:
         for i,line in enumerate(get_lines(filename)):
             words = line.split(' ')
+            if annotate: print('\n// '+line, file=f)
             if len(words) == 1:
                 print(parse_arithmetic(words, i), file=f)
             elif len(words) == 3:
                 print(parse_memory(words), file=f)
             else:
                 raise ValueError('Line not recognized:', line)
-
-def get_command(cmd):
-    return
 
 def parse_arithmetic(words, n):
     return arithmetic_operations[words[0]](n)
@@ -219,8 +174,30 @@ def parse_memory(words):
     else:
         return pop(words[1], words[2])
 
-# translator('./StackArithmetic/StackTest/StackTest.vm')
-# translator('./StackArithmetic/SimpleAdd/SimpleAdd.vm')
-# translator('./MemoryAccess/BasicTest/BasicTest.vm')
-translator('./MemoryAccess/PointerTest/PointerTest.vm')
-translator('./MemoryAccess/StaticTest/StaticTest.vm')
+if __name__=='__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='VM to Hack Translator.')
+    parser.add_argument('filename', type=str, nargs='*',
+                        help='Filenames to process.')
+    parser.add_argument('--outputs', '-o', type=str, nargs='+',
+                        help='Filenames of the outputs.')
+    parser.add_argument('--test', action='store_true',
+                        help='Generate test files.')
+    parser.add_argument('--annotate', action='store_true',
+                        help='Annotate the asm file with comments.')
+
+    args = parser.parse_args()
+
+    if args.test:
+        translator('./StackArithmetic/StackTest/StackTest.vm', annotate=args.annotate)
+        translator('./StackArithmetic/SimpleAdd/SimpleAdd.vm', annotate=args.annotate)
+        translator('./MemoryAccess/BasicTest/BasicTest.vm', annotate=args.annotate)
+        translator('./MemoryAccess/PointerTest/PointerTest.vm', annotate=args.annotate)
+        translator('./MemoryAccess/StaticTest/StaticTest.vm', annotate=args.annotate)
+
+    if args.outputs is None: args.outputs = [None]*len(args.filename)
+    assert len(args.filename) == len(args.outputs)
+
+    for fn, fo in zip(args.filename, args.outputs):
+        translator(fn, fo, annotate=args.annotate)
